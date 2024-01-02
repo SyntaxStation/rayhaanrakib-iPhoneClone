356d3f1a5ec4085258e4ca82e505b30547f21dac
developedbyrayhan <islamraihanofficial@gmail.com>
2024-01-02 16:07:30 +0600

    Added Highlights Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import Highlights from './components/Highlights';
 
 const App = () => {
   return (
     <main className="bg-black">
       <Nav />
       <Hero />
+      <Highlights />
     </main>
   );
 };
